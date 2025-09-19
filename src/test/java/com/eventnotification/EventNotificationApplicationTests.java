205c67201167b5cdb3602ee105716e978a1bbf86
Nikhil Dethe <dethenikhil7578@gmail.com>
2025-09-19 08:50:56 +0530

    feat : Backend setup using springboot

---
+++ src/test/java/com/eventnotification/EventNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
